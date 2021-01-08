27ecd667bda2fc6c0afeb421f00f2fff1630bb3e
ChaunceyCXC <chenxucan1991@gmail.com>
2021-01-08 07:56:24 -0500

    Add CSV FORMAT

--- Parser.py
+++ Parser.py
@@ -2,10 +2,11 @@ import json
 from bs4 import BeautifulSoup
 import os
 import csv
+import datetime
 
 
 def write_to_json_file(file_path, data):
-    with open(file_path, 'a',encoding='utf-8') as fp:
+    with open(file_path, 'a', encoding='utf-8') as fp:
         json.dump(data, fp)
         fp.write("\n")
 
@@ -19,26 +20,29 @@ def write_to_csv_file(file_path, data):
 class parser:
     def __init__(self):
         self.source_dir = "C:/Users/leoac/Desktop/ChatExport_29_05_2020"  # source file folder from telegram
-        self.output_file_json = "C:/Users/leoac/Desktop/ChatExportFilter_29_05_2020.json"  # output1: parsed result
-        self.output_file_csv = "C:/Users/leoac/Desktop/ChatExportFilter_29_05_2020.csv"
+        self.output_file_json = "C:/Users/leoac/Desktop/groupChat/ChatExportFilter_29_05_2020.json"  # output1: parsed result
+        self.output_file_csv = "C:/Users/leoac/Desktop/groupChat/ChatExportFilter_29_05_2020.csv"
         self.dic_file_path = "C:/Users/leoac/Desktop/dict.json"  # outout2: usernmae to number dictionary
-        self.have_text = False
+        self.have_text = True
         self.remove_deleted_account = True
-        self.remove_consequent = True
+        self.remove_consequent = False
         self.last_username = ""
         self.save_as_csv = True
+        self.sequence_range = 1
 
     def parse(self):
         csv_columns = []
         if self.have_text:
-            csv_columns = ['date', 'username', 'text']
+            csv_columns = ['sequenceID', 'date', 'username', 'text']
         else:
-            csv_columns = ['date', 'username']
+            csv_columns = ['sequenceID', 'date', 'username']
         files = os.listdir(self.source_dir)
         files.sort()
         dict = {}
         all_messages = [csv_columns]
         index = 0
+        sequenceID = 0
+        lastDate = "01.01.2000 00:00:00"
         for file in files:
             if file[-4:] == "html":
                 file_path = self.source_dir + "/" + file
@@ -47,9 +51,6 @@ class parser:
                 a_products_desc_page.close()
                 history = a_beautiful_soup.find('div', {"class": "history"})
                 divs = history.find_all('div', class_="body")
-                date = ""
-                username = ""
-                text = ""
                 for div in divs:
                     if div["class"] == ['body']:
                         date_div = div.find("div", class_="pull_right date details")
@@ -57,6 +58,15 @@ class parser:
                         text_div = div.find("div", class_="text")
                         if date_div is not None:
                             date = date_div["title"]
+                            y_date = datetime.datetime.strptime(date, '%d.%m.%Y %H:%M:%S')
+                            x_date = datetime.datetime.strptime(lastDate, '%d.%m.%Y %H:%M:%S')
+                            diff = y_date - x_date
+                            days, seconds = diff.days, diff.seconds
+                            diff_hours = days * 24 + seconds / 3600
+                            if diff_hours > self.sequence_range:
+                                sequenceID = sequenceID + 1
+                            lastDate = date
+
                         else:
                             continue
                         if name_div is not None:
@@ -74,11 +84,11 @@ class parser:
                             if username == self.last_username:
                                 continue
                         if self.have_text:
-                            a_new_message = {"date": date, "username": username, "text": text}
-                            all_messages.append([date, username, text])
+                            a_new_message = {"sequenceID": sequenceID, "date": date, "username": username, "text": text}
+                            all_messages.append([sequenceID, date, username, text])
                         else:
-                            a_new_message = {"date": date, "username": username}
-                            all_messages.append([date, username])
+                            a_new_message = {"sequenceID": sequenceID, "date": date, "username": username}
+                            all_messages.append([sequenceID, date, username])
                         if not self.save_as_csv:
                             write_to_json_file(self.output_file_json, a_new_message)
                         self.last_username = username
